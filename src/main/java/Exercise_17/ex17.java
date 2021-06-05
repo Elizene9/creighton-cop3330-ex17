a1c538e6ee101a212fa25bc0ae731f0d1d056dfa
Elizene9 <83979666+Elizene9@users.noreply.github.com>
2021-06-05 12:35:35 -0400

    Updated ex17

--- src/main/java/Exercise_17/ex17.java
+++ src/main/java/Exercise_17/ex17.java
@@ -27,63 +27,28 @@ Prevent the user from entering non-numeric values.
  */
 package Exercise_17;
 import java.util.InputMismatchException;
+import java.util.*;
 import java.util.Scanner;
 
 public class ex17 {
-
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Scanner inp = new Scanner(System.in);
-        Scanner in = new Scanner(System.in);
-        Scanner Alc = new Scanner(System.in);
-        double weight = 0;
+
         System.out.print("What is your weight in pounds? ");
-           do {
-               try {
-                   weight = input.nextDouble();
-                   break;
-               } catch (InputMismatchException e) {
-                   System.out.println("\nInvalid input\nEnter a valid numeric value");
-                   input.next();
-               }
-           } while (!(input.hasNextDouble()));
+
+        double weight = ex_17();
 
         System.out.print("\nEnter the adr for your gender, .73 for males and .66 for females: ");
-        double gender = 0;
-        do {
-            try {
-                gender = in.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("\nInvalid input\nEnter a valid numeric value");
-                in.next();
-            }
-        } while (!(in.hasNextDouble()));
+        double gender = ex_17();
+
 
         System.out.print("\nWhat is total amount of alcohol consumed in ounces? ");
-        double alc = 0;
-        do {
-            try {
-                alc = Alc.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("\nInvalid input\nEnter a valid numeric value");
-                Alc.next();
-            }
-        } while (!(Alc.hasNextDouble()));
+        double alc = ex_17();
 
         System.out.print("\nHow many hours since last drink? ");
-        int time = 0;
-        do {
-            try {
-                time = inp.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("\nInvalid input\nEnter a valid numeric value");
-                inp.next();
-            }
-        } while (!(inp.hasNextInt()));
-        double BAC = (alc * 5.14 / weight * gender) - .15 * time;
+        double time = ex_17();
+
+        double BAC = (alc * 5.14 / weight * gender) - .015 * time;
 
         if (BAC >= .08)
             System.out.printf("\nYour BAC is %.2f\nIt's not legal for you to drive.", BAC);
@@ -92,6 +57,21 @@ public class ex17 {
             System.out.printf("\nYour BAC is %.2f\nIt's legal for you to drive.", BAC);
 
         else
-            System.out.print("\nYour BAC is 0\nIt's legal for you to drive.");
+            System.out.printf("\nYour BAC is %.2f\nIt's legal for you to drive.", BAC);
     }
+
+
+public static double ex_17() {
+
+    while (true) {
+
+        try {
+            return input.nextDouble();
+        }
+        catch (InputMismatchException e) {
+            input.next();
+            System.out.println("\nInvalid input, enter a numeric value");
+        }
+    }
+}
 }
